09c239b9afe09343986528df21719d2062054481
Vivek <vivek@medianv.com>
2025-08-07 18:10:28 +0530

    Changes in code format

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -10,25 +10,25 @@ interface Posts {
 @Injectable()
 export class PostService {
 
-  private post : Posts[] = [];
-  private nextId : number = 1;
+  private post: Posts[] = [];
+  private nextId: number = 1;
 
-  create(dto : CreatePostDto) {
-   const post = {id : this.nextId++,...dto};
-   this.post.push(post);
-   return post;
+  create(dto: CreatePostDto) {
+    const post = { id: this.nextId++, ...dto };
+    this.post.push(post);
+    return post;
   }
 
   findAll(): Posts[] {
     return this.post;
   };
 
-  findOne( id : number): Posts {
-    console.log("id",id)
-     const postData : any = this.post.filter((f : any) => f.id == id);
+  findOne(id: number): Posts {
+    console.log("id", id)
+    const postData: any = this.post.filter((f: any) => f.id == id);
 
-     if(!postData.lenght) throw new NotFoundException(`No data get on this id : ${id}`)
-    return postData ;
+    if (!postData.length) throw new NotFoundException(`No data get on this id : ${id}`)
+    return postData;
   }
 
 }
